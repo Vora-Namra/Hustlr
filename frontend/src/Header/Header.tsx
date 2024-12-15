87913c90fa9d22db177c86e14416ce594ec471ca
Namra Vora <voranamra625@gmail.com>
2024-12-15 16:54:14 +0530

    finishing the complete header

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {IconAsset} from "@tabler/icons-react"
+import {IconAsset, IconBell, IconSettings} from "@tabler/icons-react"
+import { Avatar } from '@mantine/core'
 
 function Header() {
   return (
@@ -9,11 +10,19 @@ function Header() {
             <div className='text-2xl font-semibold'>Hustlr</div>
             
         </div>
-        <div>
-            links  
+        <div className='flex gap-10 items-center text-lg'>
+            <a href="">Find Job</a>
+            <a href="">Find Talent</a>
+            <a href="">Uplaod jobs</a> 
+            <a href="">About Us</a>
         </div>
-        <div>
-            profile
+        <div className='flex gap-8 items-center'>
+            <IconBell/>
+            <div className='flex items-center gap-2'>
+              <div>John</div>
+              <Avatar src="avatar.png" alt="it's me"/>
+            </div>
+            <IconSettings/>
         </div>
 
     </div>
