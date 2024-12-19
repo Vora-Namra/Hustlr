3d4fe0434168ee55e416661a29a393739f9ce2bb
Namra Vora <voranamra625@gmail.com>
2024-12-19 18:27:26 +0530

    adding working & process for Home page

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Header from '../Header/Header'
 import DreamJob from '../LandingPage/DreamJob'
 import Companies from '../LandingPage/Companies'
 import JobCategory from '../LandingPage/JobCategory'
+import Working from '../LandingPage/Working'
 function HomePage() {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
@@ -10,6 +11,7 @@ function HomePage() {
     <DreamJob/>
     <Companies/>
     <JobCategory/>
+    <Working/>
     </div>
   )
 }
