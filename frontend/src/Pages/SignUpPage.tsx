8c538c2862cc91874b7936c6e1ea97e6bbe636a2
Vraj Modi <modivraj1975@gmail.com>
2025-01-19 11:19:32 +0530

    adding login page

--- frontend/src/Pages/SignUpPage.tsx
+++ frontend/src/Pages/SignUpPage.tsx
@@ -1,10 +1,11 @@
-import { Divider } from "@mantine/core"
 import { IconAnchor } from "@tabler/icons-react"
 import { SignUp } from "../SignUpLogin/SignUp"
+import { Login } from "../SignUpLogin/Login"
 
 export const SignUpPage=()=>{
-    return <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] px-4">
-      <div className="w-[100vw] h-[100vh] flex">
+    return <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] px-4 overflow-hidden">
+      <div className="w-[100vw] h-[100vh] flex [&>*]:flex-shrink-0">
+        <Login/>
         <div className="w-1/2 h-full rounded-r-[200px] bg-mine-shaft-900 flex items-center gap-5 justify-center flex-col">
         <div className='flex gap-1 items-center text-bright-sun-400 ml-6'>
             <IconAnchor className='h-16 w-16' stroke={2}/>
